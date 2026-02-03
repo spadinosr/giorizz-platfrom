11d1e1c8a8269342389833ffcfedc390d1de1939
spadinosr <80324590+spadinosr@users.noreply.github.com>
2026-02-03 23:00:27 +0100

    Update supabase.js

--- public/scripts/supabase.js
+++ public/scripts/supabase.js
@@ -3,10 +3,4 @@
 const SUPABASE_URL = "https://ysgebcbptgjlpbgnlcjt.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzZ2ViY2JwdGdqbHBiZ25sY2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5OTMzNTUsImV4cCI6MjA4NTU2OTM1NX0.R-M89T_wNR-JvY2wtFRl4BDau63kl5Ko_8yYLSngB2M";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// Controllo sessione
-async function checkSession() {
-  const { data } = await supabase.auth.getSession();
-  return data.session;
-}
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
