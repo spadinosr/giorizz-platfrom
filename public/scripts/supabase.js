6af8ce248ff67c41547e6e00dae5b9a06bb7933b
spadinosr <80324590+spadinosr@users.noreply.github.com>
2026-02-03 22:59:12 +0100

    Update supabase.js

--- public/scripts/supabase.js
+++ public/scripts/supabase.js
@@ -1,7 +1,7 @@
 // SUPABASE INIT
 
 const SUPABASE_URL = "https://ysgebcbptgjlpbgnlcjt.supabase.co";
-const SUPABASE_ANON_KEY = "INSERISCI_LA_TUA_ANON_KEY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzZ2ViY2JwdGdqbHBiZ25sY2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5OTMzNTUsImV4cCI6MjA4NTU2OTM1NX0.R-M89T_wNR-JvY2wtFRl4BDau63kl5Ko_8yYLSngB2M";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
